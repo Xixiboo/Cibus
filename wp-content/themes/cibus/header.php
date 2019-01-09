fdec4902381122edbdff489b5d72a309eed50772
aurore_zander <aurorezander@gmail.fr>
2019-01-09 14:51:27 +0100

    hey

---
+++ wp-content/themes/cibus/header.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+
+<html lang="fr">
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>Cibus</title>
+
+<!-- CSS de Bootstrap -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+
+<!-- Feuille CSS -->
+    <link rel="stylesheet" href="style.css">
+    <?php wp_head(); ?>
+</head>
+<body>
+    <div class="header">
+    <!-- banniere -->
+        <div class="banniere">
+
+        </div>
+        <nav id="nav">
+            <a  href="#">Accueil</a>
+            <a href="#">Articles</a>
+            <a href="#">Contacts</a>
+            <a href="#">À Propos</a>
+        </nav>
+    </div>
+<div class="container">
--- wp-content/themes/cibus/index.php
+++ wp-content/themes/cibus/index.php
@@ -19,14 +19,9 @@
 </head>
 
 <body>
-    <header>
-        
-    </header>
-    
-    <footer>
-
-    </footer>
-
+    <?php get_header(); ?>
+    <?php get_content(); ?>
+    <?php get_footer(); ?>
 
 <!-- JavaScript de Bootstrap et jQuery -->
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
