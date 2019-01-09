88b86f1c3058336a8c26eefac0df83af02fa0435
aurore_zander <aurorezander@gmail.fr>
2019-01-09 15:40:18 +0100

    b

--- wp-content/themes/cibus/index.php
+++ wp-content/themes/cibus/index.php
@@ -11,8 +11,6 @@
 
     <title>Cibus</title>
 
-<!-- CSS de Bootstrap -->
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
 
 <!-- Feuille CSS -->
     <link rel="stylesheet" href="style.css">
