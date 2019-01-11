2e25ea5e6c7fcd3e657c3ca417a098e22ae7c9c8
yuzume <y.cecile0@gmail.com>
2019-01-11 10:20:41 +0100

    header,footer,css

--- wp-content/themes/cibus/header.php
+++ wp-content/themes/cibus/header.php
@@ -16,21 +16,24 @@
 
 <!-- Feuille CSS -->
     <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.3/css/all.css" integrity="sha384-UHRtZLI+pbxtHCWp1t77Bi1L4ZtiqrqD80Kn4Z8NTSRyMA2Fd33n5dQ8lWUE00s/" crossorigin="anonymous">
+
+
     
     <?php wp_head(); ?>
 </head>
 <body>
     <div class="header">
+
     <!-- banniere -->
         <div class="banniere">
-
+            <img src="<?php echo get_bloginfo('template_url') ?>../img/ban.jpg"/>
         </div>
+
         <nav id="nav">
-            <?php wp_nav_menu( array( 'theme_location' => 'menu-principal' ) ); ?>
-            <li><a href="#">Accueil</a></li>
-            <li><a href="#">Articles</a></li>
-            <li><a href="#">Contacts</a></li>
-            <li><a href="#">A Propos</a></li>
+        <?php wp_nav_menu( array( 'theme_location' => 'menu-principal' ) ); ?>
+
         </nav>
+
     </div>
 <div class="container">
